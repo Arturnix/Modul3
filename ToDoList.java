9a49d8735d0b3228002d48bf2fdf5990609c258a
Arturnix <artur.zgodka.programista@gmail.com>
2023-12-05 22:51:08 +0100

    Add size of array and index check before removing the element

--- ToDoList.java
+++ ToDoList.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class ToDoList {
 
     ArrayList<String> toDoList = new ArrayList<>();
-    
+
     public ToDoList() {
         ArrayList<String> toDoList = new ArrayList<>();
     }
@@ -20,6 +20,11 @@ public class ToDoList {
     }
 
     public void remove(int number) {
+
+        if((number > toDoList.size()-1) || (number < toDoList.size()-1)) {
+            System.out.println("Podana pozycja spoza zakresu listy! Podaj poprawną wartość z zakresu: 0 - " + (toDoList.size()-1));
+            System.exit(0);
+        }
 
         toDoList.remove(number);
     }
