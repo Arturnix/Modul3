687884d270d06cfe75f3f26859e43bbdc847ef12
Arturnix <artur.zgodka.programista@gmail.com>
2023-12-05 22:22:50 +0100

    Zadanie 2

---
+++ ToDoList.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class ToDoList {
+
+    ArrayList<String> toDoList = new ArrayList<>();
+    public ToDoList() {
+        ArrayList<String> toDoList = new ArrayList<>();
+    }
+
+    public void add(String task) {
+        toDoList.add(task);
+    }
+
+    public void print() {
+
+            for(String i : toDoList) {
+                System.out.println(toDoList.indexOf(i) + ": " + i);
+            }
+
+
+    }
+
+    public void remove(int number) {
+
+        toDoList.remove(number);
+    }
+}
