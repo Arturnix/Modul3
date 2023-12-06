68351a9c87161111f6ede5c9f54c939e80129ca8
Arturnix <artur.zgodka.programista@gmail.com>
2023-12-06 14:43:14 +0100

    Add Main

---
+++ Main.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Zadanie 1
+        /*HashSet<Integer> numbers = new HashSet<>();
+
+        numbers.add(12);
+        numbers.add(12);
+
+        for(Integer i : numbers) {
+            System.out.println(i);
+        }*/
+
+        // Zadanie 2
+        /*ToDoList toDoList = new ToDoList();
+        toDoList.add("Praca");
+        toDoList.add("Wyjscie do sklepu");
+        toDoList.print();
+        toDoList.remove(1);
+        toDoList.print();*/
+
+        // Zadanie 3
+        /*String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem lectus, varius et lacus sagittis, viverra cursus diam. Suspendisse gravida dignissim nunc, quis mattis diam tempus at.";
+        CountWordsInText wordsInText = new CountWordsInText();
+        wordsInText.countWords(text);*/
+
+        // Zadanie 4
+       /* List<Student> students = new ArrayList<>();
+        Student student1 = new Student(20, 185, "Ron", "Flex");
+        Student student2 = new Student(19, 180, "Jon", "Bet");
+        Student student3 = new Student(24, 179, "Alex", "Kolw");
+        Student student4 = new Student(17, 182, "Franz", "Kolw");
+        Student student5 = new Student(23, 178, "Henry", "Bet");
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+        students.add(student5);
+
+        System.out.println("Before sorting: ");
+        for(int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i).getLastName() + " " + students.get(i).getAge());
+        }
+
+        Collections.sort(students);
+
+        System.out.println("After sorting: ");
+        for(int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i).getLastName() + " " + students.get(i).getAge());
+        }
+*/
+    }
+}
