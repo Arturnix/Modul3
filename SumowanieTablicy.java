3c2f34b9b3a98e69b68048208b7cd7ec758b406f
Arturnix <artur.zgodka.programista@gmail.com>
2023-12-08 14:41:58 +0100

    Add exceeded type range while adding table elements

--- SumowanieTablicy.java
+++ SumowanieTablicy.java
@@ -1,18 +1,29 @@
 public class SumowanieTablicy {
     public static void main(String[] args) {
 
-        int[] numbers = {1, 5, 11};
+        int[] numbers = {2147483647, 5, 11};
 
         System.out.println(sumArray(numbers));
     }
 
+
     public static int sumArray(int[] numbers) {
-
+        boolean isNegative = false;
         int sum = 0;
-        for(int i = 0; i < numbers.length; i++) {
-            sum += numbers[i];
-        }
 
-        return sum;
+
+            for (int i = 0; i < numbers.length; i++) {
+                sum += numbers[i];
+                if(numbers[i] < 0) {
+                    isNegative = true;
+                }
+            }
+
+            if((sum < 0) && isNegative == false) {
+                System.out.println("Suma podanych liczb przekracza zakres typu danych.\n" +
+                        "Podaj mniejsze wartości.");
+                return 0;
+            }
+            return sum;
     }
 }
